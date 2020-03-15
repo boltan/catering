a926b5469b416b4ad2d0274b3f40c5ecc4425b10
Yehonatan <boltanskyyehonatan@gmail.com>
2020-03-16 00:08:31 +0200

    about page more bug fixes

--- js/whatsApp-button-shower.js
+++ js/whatsApp-button-shower.js
@@ -2,8 +2,8 @@
 function WhatStartUp() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById('whatsappIcon').style.display = 'inline-block';
-        document.getElementById('aboutWhatsappLink').style.display = 'block';
-        document.getElementById('aboutPhoneLink').style.display = 'block';
+        document.getElementById('aboutWhatsappLink').style.display = 'inline-block';
+        document.getElementById('aboutPhoneLink').style.display = 'inline-block';
         document.getElementById('aboutWhatsappSpan').style.display = 'none';
         document.getElementById('aboutPhoneSpan').style.display = 'none';
     }
