6ffd27c916a5242eb76a986a9e4af4767ac5beb2
Yehonatan <boltanskyyehonatan@gmail.com>
2020-03-15 21:22:11 +0200

    hopefully mobile bug fixes

--- js/whatsApp-button-remover.js
+++ js/whatsApp-button-remover.js
@@ -1,3 +1,4 @@
+
 function StartUp() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById('whatsappIcon').style.display = 'inline-block;';
